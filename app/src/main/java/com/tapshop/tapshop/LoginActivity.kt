20cccee2b7335230b3567ebd09708310a1042d0d
Ashish Gupta <guptaashish190@gmail.com>
2019-05-11 20:49:46 +0530

    changed google signin buttoin

--- app/src/main/java/com/tapshop/tapshop/LoginActivity.kt
+++ app/src/main/java/com/tapshop/tapshop/LoginActivity.kt
@@ -2,13 +2,22 @@ package com.tapshop.tapshop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private var username: String? = null
+    private var password: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        username = username_textinput.toString()
+        password = password_textinput.toString()
+
     }
 
 }
