83aff0b21edff42aeb01eacfe8f15386cb792516
Ashish Gupta <guptaashish190@gmail.com>
2019-05-12 00:35:35 +0530

    register user func

--- app/src/main/java/com/tapshop/tapshop/RegisterActivity.kt
+++ app/src/main/java/com/tapshop/tapshop/RegisterActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +23,20 @@ class RegisterActivity : AppCompatActivity() {
             val username: String = username_textinput.text.toString()
             val pass:String = password_textinput.text.toString()
             val repass: String = re_password_textinput.text.toString()
-
-            if(validateUserInput(username, pass, repass)){
-                println("Yesss")
+            val email: String = email_textinput.text.toString()
+            if(validateUserInput(username, pass, repass, email)){
+                val auth = FirebaseAuth.getInstance()
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+                    if(it.isSuccessful){
+                        Log.d("Register", "Registered")
+                        Toast.makeText(baseContext, "Registered User",
+                            Toast.LENGTH_SHORT).show()
+                        finish()
+                    }else{
+                        // Cancel Display error{
+                        Log.d("Register", "createUserWithEmail:failure")
+                    }
+                }
             }else{
                 println("Nopeee")
             }
@@ -30,6 +45,7 @@ class RegisterActivity : AppCompatActivity() {
         setTextChangeListener(username_textinput, username_layout)
         setTextChangeListener(password_textinput, password_layout)
         setTextChangeListener(re_password_textinput, re_password_layout)
+        setTextChangeListener(email_textinput, email_layout)
 
     }
 
@@ -49,18 +65,28 @@ class RegisterActivity : AppCompatActivity() {
         })
     }
 
-    private fun validateUserInput( username: String,  password: String, repassword: String): Boolean{
+    private fun validateUserInput( username: String,  password: String, repassword: String, email: String): Boolean{
         var valid = true
 
         username_layout.error = null
         password_layout.error = null
         re_password_layout.error = null
+
+        if(email.isEmpty()){
+            email_layout.error = "Provide an email"
+            valid = false
+        }
+
         if(username.isEmpty()){
             username_layout.error = "Provide a username"
             valid = false
         }
         if(password.isEmpty()){
             password_layout.error = "Provide a password"
+            valid = false
+        }
+        if(password.length < 6){
+            password_layout.error = "Password Length should be atleast 6"
             valid = false
         }
         if(repassword.isEmpty()){
@@ -70,6 +96,7 @@ class RegisterActivity : AppCompatActivity() {
 
         if(password != repassword){
             re_password_layout.error = "Passwords do not match"
+            valid = false
         }
 
         return(valid)
